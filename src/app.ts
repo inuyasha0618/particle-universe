cb276428be4e561ac76ff26db923036f36d0b1c8
inuyasha0618 <chenjiandiwang@qq.com>
2019-03-22 14:42:26 +0800

    合并geometery成功

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,12 @@
 import * as THREE from 'three';
 import RenderLooper from 'render-looper';
+import { BufferGeometryUtils } from './libs/BufferGeometeryUtils';
 const OrbitControls = require('three-orbitcontrols')
 
 var OBJLoader = require('three-obj-loader');
 OBJLoader(THREE);
 
 const scene: THREE.Scene = new THREE.Scene();
-// let ball = new THREE.SphereGeometry(40, 30, 30);
-// let pointMaterial = new THREE.PointsMaterial({
-//     color: 0xffffff,
-//     size: 2
-// })
-
-// const particleSystem = new THREE.Points(ball, pointMaterial);
-// scene.add(particleSystem);
 
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.setZ(200);
@@ -34,28 +27,25 @@ controls.maxDistance = Infinity;
 this.enableZoom = true; // Set to false to disable zooming
 this.zoomSpeed = 1.0;
 
-console.log(THREE.OBJLoader);
+let totalBufferGeometry: THREE.BufferGeometry = new THREE.BufferGeometry();
 const loader = new THREE.OBJLoader();
-loader.load('./models/BaoLi_O.obj', function(object) {
-    for (let child of object.children) {
-        const geo = child.geometry;
-        let pointMaterial = new THREE.PointsMaterial({
-            color: 0xffffff,
-            size: 0.1
-        })
-    
-        const particleSystem = new THREE.Points(geo, pointMaterial);
-        scene.add(particleSystem);
-    }
 
+loader.load('./models/o.obj', function(object) { 
+    // 将所有的geometry合并为一个大的
+    totalBufferGeometry = BufferGeometryUtils.mergeBufferGeometries(object.children.map(child => child.geometry), false);
+
+    let pointMaterial = new THREE.PointsMaterial({
+        color: 0xffffff,
+        size: 0.1
+    })
+
+    const particleSystem = new THREE.Points(totalBufferGeometry, pointMaterial);
+    scene.add(particleSystem);
+    console.log(totalBufferGeometry);
 })
 
 new RenderLooper(() => {
     scene.rotation.y += 0.01;
-    scene.traverse(function(obj) {
-        console.log(obj);
-        return;
-    })
     renderer.render(scene, camera);
     
 }).start()
